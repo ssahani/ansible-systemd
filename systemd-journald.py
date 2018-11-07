532a00f7d8ef76b8e31d1083e63e9cf316701f20
Susant Sahani <ssahani@gmail.com>
2018-11-07 14:41:23 +0530

    Update Licence

--- systemd-journald.py
+++ systemd-journald.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+# SPDX-License-Identifier: LGPL-2.1+
 # (c) 2018, Susant Sahani <susant@redhat.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
 
